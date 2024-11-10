69276bd4bd8c032ef6db72a5c3eefbc35ae5ae17
DK <163239985+DivyaK2097@users.noreply.github.com>
2024-11-10 22:07:30 +0530

    Update Base.java

--- Base.java
+++ Base.java
@@ -3,26 +3,28 @@ package com.exercise.base;
 import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
 
 public class Base {
-	protected WebDriver driver;
-	String url = "https://the-internet.herokuapp.com/";
-	
-	@BeforeMethod
-	public void setup() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Windows\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get(url);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(160));
-	}
-	
-	@AfterMethod
-	public void tearDown() {
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(160));
-		driver.close();
-		driver.quit();
-	}
+    protected WebDriver driver;
+    String url = "https://the-internet.herokuapp.com/";
+
+    @BeforeMethod
+    public void setup() {
+        //Setup WebDriver (ensure correct ChromeDriver path)
+        System.setProperty("webdriver.chrome.driver", "C:\\Windows\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(url);
+
+        //Set implicit wait duration 
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterClass
+    public void tearDown() {
+        //Close and quit WebDriver session
+        driver.quit();
+    }
 }
